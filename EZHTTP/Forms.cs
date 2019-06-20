a025bb3449fe03f31f66ce4fdb7dcc0745e45ad0
Tom Van Howe <tom@vanhowe.com>
2019-06-20 14:46:48 +0100

    Beta

--- EZHTTP/Forms.cs
+++ EZHTTP/Forms.cs
@@ -7,13 +7,17 @@ namespace EZHTTP {
     class Forms {
         public static Dictionary<string, string> ParseForm(string query) {
             var p = HttpUtility.ParseQueryString(query);
-            return p.AllKeys.ToDictionary(k => k, k => p[k]);
+            return p.AllKeys.ToDictionary(k => k, k => p[k], StringComparer.OrdinalIgnoreCase);
         }
         public static T ParseForm<T>(string query) where T : new() {
             T result = new T();
             var fields = typeof(T).GetFields();
-
-            //fields[0].
+            var dict = ParseForm(query);
+            foreach (var field in fields) {
+                if (dict.TryGetValue(field.Name, out string value)) {
+                    field.SetValue(result, value);
+                }
+            }
             return result;
         }
     }
